d94df5e863a7ea55500394c384da3c26e5e4bae4
sun <sun@sunbodeMacBook-Air.local>
2017-11-28 17:49:58 +0800

    demo

---
+++ examples/CoreEditTextDemo/CoreEditTextDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CoreEditTextDemo
+//
+//  Created by sun on 2017/11/28.
+//  Copyright © 2017年 irunthink. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
