25853d0e104a2a9c9176a18486412c540e132bda
sun <sun@126.com>
2018-01-03 20:23:43 +0800

    update

--- examples/normal/CoreEditTextDemo/CoreEditTextDemo/CoreEditText.framework/Versions/A/Headers/SunDebugs.h
+++ examples/normal/CoreEditTextDemo/CoreEditTextDemo/CoreEditText.framework/Versions/A/Headers/SunDebugs.h
@@ -6,28 +6,40 @@
 //  Copyright © 2016年 sun. All rights reserved.
 //
 /*
+ 系统自己统计app使用过程中的应用崩溃信息，并存入沙盒中，方便查看，查找原因，并及时修正Bugs。
+
  使用方法
- 1、头文件声明：
+ 1、头文件声明(AppDelegate.m文件)：
  @property (nonatomic) NSUncaughtExceptionHandler *uncaughtExceptionHandler;
- 
+
+
  2、didFinishLaunchingWithOptions方法中加入：
+
  //异常处理
  self.uncaughtExceptionHandler = NSGetUncaughtExceptionHandler();
  NSSetUncaughtExceptionHandler(&UncaughtExceptionHandler);
+
+
  3、applicationDidBecomeActive:(UIApplication *)application
- 进入程序时开始收集bugs
- 【StartReadSunBugs;】
+
+ //进入程序时开始收集bugs
+ StartSaveSunBugs;
+
+
  4、applicationDidEnterBackground:(UIApplication *)application
- 程序进入后台时暂停收集bugs
- 【PauseReadSunBugs;】
+
+ //程序进入后台时暂停收集bugs
+ PauseSaveSunBugs;
  
  */
 
 #import <Foundation/Foundation.h>
 
-#define StartReadSunBugs        [[SunDebugs shareDefineBugs] readBugs]
+#define StartSaveSunBugs        [[SunDebugs shareDefineBugs] saveBugs]                  //开始记录Bugs
 
-#define PauseReadSunBugs        [[SunDebugs shareDefineBugs] pauseReadBugs]
+#define PauseSaveSunBugs        [[SunDebugs shareDefineBugs] pauseSaveBugs]             //暂停记录Bugs
+
+#define KBugsFileListArray      [[SunDebugs shareDefineBugs] BugFileListArray]          //沙盒中记录的bug文件名的数组
 
 @interface SunDebugs : NSObject
 
@@ -35,10 +47,12 @@ void UncaughtExceptionHandler(NSException *exception);
 
 void HandlingExceptionsSunHand();
 
-+(SunDebugs *)shareDefineBugs;
++ (SunDebugs *)shareDefineBugs;
 
-- (void)readBugs;
+- (void)saveBugs;
 
-- (void)pauseReadBugs;
+- (void)pauseSaveBugs;
+
+- (NSMutableArray *)BugFileListArray;
 
 @end
